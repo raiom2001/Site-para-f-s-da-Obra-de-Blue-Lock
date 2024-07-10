bd97da22a82671349cd06ef112224ee6a1ff3725
Guilherme Macena <73499324+raiom2001@users.noreply.github.com>
2024-07-09 22:38:03 -0300

    Update noticias.js

--- noticias.js
+++ noticias.js
@@ -1,29 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const Parser = require('rss-parser');
-
-const app = express();
-const parser = new Parser();
-
-// Middleware para permitir CORS
-app.use(cors());
-
-const rssUrl = 'https://news.google.com.br/rss/search?q=Blue+Lock';
-
-app.get('/noticias', async (req, res) => {
+async function fetchNoticias() {
   try {
-    const feed = await parser.parseURL(rssUrl);
+    const response = await fetch('https://organizadalock.vercel.app/noticias'); // Atualize para o URL do seu projeto Vercel
+    if (!response.ok) {
+      throw new Error('Erro na requisição: ' + response.statusText);
+    }
+    const noticias = await response.json();
+    const noticiasContainer = document.getElementById('noticias');
+    noticiasContainer.innerHTML = ''; // Limpa os resultados anteriores
 
-    // Filtrar ou processar as notícias conforme necessário
-    const noticias = feed.items.map(item => ({
-      title: item.title,
-      link: item.link,
-      contentSnippet: item.contentSnippet
-    }));
-
-    res.json(noticias); // Envia as notícias em formato JSON
+    noticias.forEach(noticia => {
+      const noticiaElement = document.createElement('div');
+      noticiaElement.classList.add('noticia'); // Adiciona a classe 'noticia' para aplicar estilos CSS
+      noticiaElement.innerHTML = `<h2>${noticia.title}</h2><p>${noticia.contentSnippet}</p><a href="${noticia.link}" target="_blank">Leia mais</a>`;
+      noticiasContainer.appendChild(noticiaElement);
+    });
   } catch (error) {
     console.error('Erro ao buscar notícias:', error);
-    res.status(500).send('Erro ao buscar notícias: ' + error.message);
   }
-});
+}
+
+document.addEventListener('DOMContentLoaded', fetchNoticias);
