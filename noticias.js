e48f16bccf1a09f3c0adb6c1e711a54f7b828277
Guilherme Macena <73499324+raiom2001@users.noreply.github.com>
2024-07-27 15:46:36 -0300

    Update noticias.js

--- noticias.js
+++ noticias.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const Parser = require('rss-parser');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 const parser = new Parser();
 
 // Middleware para permitir CORS
